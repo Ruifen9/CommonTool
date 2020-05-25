57cf07d76ba7e0a66a0d751bd32d5d56813f04f2
ruifo <cg-zhong@qq.com>
2020-05-25 16:09:28 +0800

    加入Beacon，支持非标准的广播数据

--- app/src/main/java/com/ruifen9/commontool/MainActivity.kt
+++ app/src/main/java/com/ruifen9/commontool/MainActivity.kt
@@ -2,7 +2,8 @@ package com.ruifen9.commontool
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jakewharton.rxbinding4.view.clicks
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        Observable.just(0)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({}, {})
     }
 }
--- app/uicomponents/src/main/java/com/ruifen9/uicomponents/MyUIKtx.kt
+++ app/uicomponents/src/main/java/com/ruifen9/uicomponents/MyUIKtx.kt
@@ -1,10 +1,13 @@
 package com.ruifen9.uicomponents
 
 import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+
 
 fun ViewGroup.newAdapterItemView(@LayoutRes itemRes: Int): View {
     return LayoutInflater.from(this.context).inflate(itemRes, this, false)
@@ -18,4 +21,16 @@ fun Int.dpToPx(context: Context): Int {
 fun Float.dpToPx(context: Context): Int {
     val scale = context.resources.displayMetrics.density
     return (this * scale + 0.5f).toInt()
+}
+
+fun Context.getScreenWidth(): Int {
+    val resources: Resources = resources
+    val dm: DisplayMetrics = resources.displayMetrics
+    return dm.widthPixels
+}
+
+fun Context.getScreenHeight(): Int {
+    val resources: Resources = resources
+    val dm: DisplayMetrics = resources.displayMetrics
+    return dm.heightPixels
 }
